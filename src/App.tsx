b0c0bc0fd05cb34f1006ad255b14cbce5482b5c8
AdrianKnudsen <adrian@knudsen.no>
2023-11-13 11:20:50 +0100

    added html & css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
 
-function App() {
-  <div></div>;
+export default function App() {
+  return (
+    <div>
+      <main>
+        <h1>
+          <span className="find">Find</span> <span className="your">your</span>
+          <br />
+          <span className="tv">next Tv</span>
+          <br />
+          <span className="adventure">Adventure</span>
+        </h1>
+        <input name="name" type="text" placeholder="Search your heart out..." />
+        <button>Search!</button>
+      </main>
+      <footer>
+        <p>“Streaming data powered by Watchmode.com"</p>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
