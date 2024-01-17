df11b6b7eb2e777c0f80efbf37948cacb2f05f6f
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2024-01-17 12:07:23 +0100

    changed the structure of the app
    moved code from app.tsx into components and moved css into modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
 import "./App.css";
-import MyApi from "./components/ApiFetch";
-import React, { useState } from "react";
+import Hero from "./components/hero/Hero";
+import SearchBar from "./components/searchBar/SearchBar";
+import Button from "./components/button/Button";
+import Footer from "./components/footer/Footer";
 
 export default function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [triggerFetch, setTriggerFetch] = useState(false);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearch = () => {
-    setTriggerFetch((prev) => !prev);
-  };
-
   return (
     <div>
       <main>
-        <h1>
-          <span className="find">Find</span> <span className="your">your</span>
-          <br />
-          <span className="tv">next Tv</span>
-          <br />
-          <span className="adventure">Adventure</span>
-        </h1>
-        <input
-          name="name"
-          type="text"
-          placeholder="Search your heart out..."
-          value={searchTerm}
-          onChange={handleInputChange}
-        />
-        <button onClick={handleSearch}>Search!</button>
+        <Hero />
+        <SearchBar />
+        <Button />
       </main>
-      <div>
-        <MyApi searchTerm={searchTerm} triggerFetch={triggerFetch} />
-      </div>
-      <footer>
-        <p>“Streaming data powered by Watchmode.com"</p>
-      </footer>
+
+      <Footer />
     </div>
   );
 }
